60502d362916fb006d8e975f36e4e738fee7a93f
nao-riku <76162238+nao-riku@users.noreply.github.com>
2022-09-18 22:46:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -54,6 +54,9 @@ def TweetId2Time(id):
     epoch = ((id >> 22) + 1288834974657) / 1000.0
     d = datetime.datetime.fromtimestamp(epoch)
     d = d.astimezone(datetime.timezone(datetime.timedelta(hours=9)))
+    return d
+    
+def TimeToStr(t_time):
     stringTime = ""
     stringTime += '{0:02d}'.format(d.hour)
     stringTime += ':'
@@ -64,7 +67,34 @@ def TweetId2Time(id):
     stringTime += '{0:03d}'.format(int(d.microsecond / 1000))
     return stringTime
 
+today_result = []
+load_res_yet = True
+
+def get_result():
+    global today_result
+    r = requests.get(os.environ['URL2'])
+    today_result = json.loads(r.json)
+
 def get_stream(headers):
+    now = datetime.datetime.now()
+    times = [
+        datetime.datetime(now.year, now.month, now.day, 0, 0, 0),
+        datetime.datetime(now.year, now.month, now.day, 3, 0, 0),
+        datetime.datetime(now.year, now.month, now.day, 7, 0, 0),
+        datetime.datetime(now.year, now.month, now.day, 11, 0, 0),
+        datetime.datetime(now.year, now.month, now.day, 15, 0, 0),
+        datetime.datetime(now.year, now.month, now.day, 19, 0, 0),
+        datetime.datetime(now.year, now.month, now.day, 23, 0, 0),
+        datetime.datetime(now.year, now.month, now.day + 1, 3, 0, 0),
+        datetime.datetime(now.year, now.month, now.day + 1, 7, 0, 0)
+    ]
+    for num in range(7):
+        if times[num].time() < now.time() <= times[num + 1].time():
+            start_time = times[num + 1]
+            end_time = times[num + 2]
+            exit_time = datetime.datetime(end_time.year, end_time.month, end_time.day, end_time.hour, 0, 20)
+
+    
     global oath
     proxy_dict = {"http": "socks5://127.0.0.1:9050", "https": "socks5://127.0.0.1:9050"}
     run = 1
@@ -77,30 +107,34 @@ def get_stream(headers):
                 for response_line in response.iter_lines():
                     if response_line:
                         json_response = json.loads(response_line)
-                        tweet_text = json_response["data"]["text"]
-                        if "@Rank334" in tweet_text or "@rank334" in tweet_text:
-                            reply_id = json_response["data"]["id"]
-                            rep_text = ""
+                        tweet_id = json_response["data"]['referenced_tweets'][0]["id"]
+                        t_time = TweetId2Time(int(tweet_id))
+                        if start_time.time() <= t_time.time() < end_time.time():
+                        
+                            tweet_text = json_response["data"]["text"]
+                            if "@Rank334" in tweet_text or "@rank334" in tweet_text:
+                                reply_id = json_response["data"]["id"]
+                                rep_text = ""
 						
-                            if 'referenced_tweets' in json_response["data"]:
-                                if json_response["data"]['referenced_tweets'][0]["type"] == "retweeted":
-                                    continue
+                                if 'referenced_tweets' in json_response["data"]:
+                                    if json_response["data"]['referenced_tweets'][0]["type"] == "retweeted":
+                                        continue
+                                    else:
+                                        rep_text = "ツイート時刻: " + TimeToStr(t_time)
                                 else:
-                                    tweet_id = json_response["data"]['referenced_tweets'][0]["id"]
-                                    rep_text = "ツイート時刻: " + TweetId2Time(int(tweet_id))
-                            else:
-                                rep_text = "ツイート時刻: " + TweetId2Time(int(reply_id)) + "\n\n順位: /"
+                                    rep_text = "ツイート時刻: " + TweetId2Time(int(reply_id)) + "\n\n順位: /"
 							
-                            print(rep_text)
-                            params = {"text": rep_text, "reply": {"in_reply_to_tweet_id": reply_id}}
-                            response = oath.post("https://api.twitter.com/2/tweets", json = params)
-                            print(response.headers["x-rate-limit-remaining"])
-                            if "status" in response.json():
-                                if response.json()["status"] == 429:
-                                    response = oath.post("https://api.twitter.com/2/tweets", json = params, proxies = proxy_dict)
+                                params = {"text": rep_text, "reply": {"in_reply_to_tweet_id": reply_id}}
+                                response = oath.post("https://api.twitter.com/2/tweets", json = params)
+                                #print(response.headers["x-rate-limit-remaining"])
+                                if "status" in response.json():
+                                    if response.json()["status"] == 429:
+                                        response = oath.post("https://api.twitter.com/2/tweets", json = params, proxies = proxy_dict)
+                                        
+                        if < t_time.time() 
 							
-                            if time.time() - start > 40:
-                                sys.exit()
+                        if exit_time.time() < t_time.time():
+                             sys.exit()
 
 
         except ChunkedEncodingError as chunkError:
@@ -126,7 +160,9 @@ def get_stream(headers):
 class ChunkedEncodingError(Exception):
     pass
 
+
 def main():
+    get_result()
     rules = get_rules()
     delete = delete_all_rules(rules)
     set = set_rules(delete)
