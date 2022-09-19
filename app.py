646fccf38c3154097d2385bd4d5672b537548149
nao-riku <76162238+nao-riku@users.noreply.github.com>
2022-09-20 00:13:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def get_result():
     load_res_yet = False
     r = requests.get(os.environ['URL2'])
     today_result = r.json()
-    print(today.result)
+    print(today_result)
     if today_result == {}:
         load_res_yet = True
     
@@ -104,7 +104,7 @@ def get_stream():
             start_time = datetime.datetime(times[num + 1].year, times[num + 1].month, times[num + 1].day, times[num + 1].hour, times[num + 1].minute, times[num + 1].second + 1)
             end_time = times[num + 2]
     start_time = datetime.datetime(now.year, now.month, now.day, now.hour, now.minute + 1, 0)
-    end_time = datetime.datetime(now.year, now.month, now.day, 0, 14, 40)
+    end_time = datetime.datetime(now.year, now.month, now.day, 0, 15, 40)
                 
     if start_time.hour != 2:
         get_result()
