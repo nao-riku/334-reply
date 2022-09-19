ef7c9949c6a0abdd76c873f0edb75aa2c79ab9b7
nao-riku <76162238+nao-riku@users.noreply.github.com>
2022-09-20 00:41:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -102,8 +102,6 @@ def get_stream():
         if com_t(times[num], now, times[num + 1]):
             start_time = datetime.datetime(times[num + 1].year, times[num + 1].month, times[num + 1].day, times[num + 1].hour, times[num + 1].minute, times[num + 1].second + 1)
             end_time = times[num + 2]
-    start_time = datetime.datetime(now.year, now.month, now.day, now.hour, now.minute, now.second + 5)
-    end_time = datetime.datetime(now.year, now.month, now.day, 2, 47, 40)
                 
     if start_time.hour != 2:
         get_result()
@@ -117,10 +115,10 @@ def get_stream():
         nonlocal start_time, end_time
         
         #if com(datetime.datetime(now.year, now.month, now.day, 22, 47, 40), start_time):
-        load_time = datetime.datetime(start_time.year, start_time.month, start_time.day - 1, 3, 34, 45)
-        r_start_time = datetime.datetime(start_time.year, start_time.month, start_time.day - 1, 3, 35, 0)
+        load_time = datetime.datetime(start_time.year, start_time.month, start_time.day, 3, 34, 45)
+        r_start_time = datetime.datetime(start_time.year, start_time.month, start_time.day, 3, 35, 0)
         start_str = start_time.date().strftime('%Y/%m/%d')
-        r_end_time = datetime.datetime(start_time.year, start_time.month, start_time.day + 0, 0, 0, 0)
+        r_end_time = datetime.datetime(start_time.year, start_time.month, start_time.day + 1, 0, 0, 0)
     
         global oath, today_result, load_res_yet
         proxy_dict = {"http": "socks5://127.0.0.1:9050", "https": "socks5://127.0.0.1:9050"}
